ccc6572bfaf44a1a6752a4e642315b0be7e84a74
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-05-21 15:20:50 -0400

    edits

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -24,7 +24,7 @@ const resolvers = {
 		},
   },
 
-  Muration: {
+  Mutation: {
     addUser: async (parent, {username, email, password}) => {
 			const user = await User.create({username, email, password});
 			const token = signToken(user);
